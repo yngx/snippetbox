2bf550382fd1ec81480a9ddf072caaec3db3430f
Xao <divxyng.edu@gmail.com>
2021-10-13 22:32:16 -0500

    Added id for show snippet

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 /*
@@ -23,6 +25,20 @@ func home(w http.ResponseWriter, r *http.Request) {
 }
 
 func showSnippet(w http.ResponseWriter, r *http.Request) {
+	// Extract the value of the id parameter from the query string and try to
+	// convert it to an integer using the strconv.Atoi() function.
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	/*
+		We pass our http.ResponseWriter to fmt.Fprintf because our http.ResponseWriter
+		satisfies the io.Writer interface that Fprintf requires.
+	*/
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+
 	/*
 		Go will automatically set the following headers for us:
 		Date, Content-Length, and Content-Type.
@@ -35,7 +51,7 @@ func showSnippet(w http.ResponseWriter, r *http.Request) {
 		`Content-Type: text/plain; charset=utf-8` unless we explicitlky
 		set the header.
 	*/
-	w.Write([]byte("Display"))
+	//w.Write([]byte("Display"))
 }
 
 func createSnippet(w http.ResponseWriter, r *http.Request) {
