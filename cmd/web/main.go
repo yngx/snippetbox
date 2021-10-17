ac94817a6bc8f25a0ca3b9a0ae4fa7d15239cc8b
Xao <divxyng.edu@gmail.com>
2021-10-16 20:49:01 -0500

    Added static files and file server

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -28,6 +28,24 @@ func main() {
 	mux.HandleFunc("/snippets", showSnippets)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
+	// Create a file server which serves files out of the "./ui/static" directory.
+	// Note that the path given to the http.Dir function is relative to the project
+	// directory root.
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
+	/*
+		Go's file server is pretty cool. It has a few really nice features:
+		* sanitizes all request paths
+		* range requests are fully supported. You can specify the range of bytes you want.
+		* Last-Modified and If-Modified-Since headers are transparently supported.
+		* Content-Type is automatically set from the file extension
+	*/
+
+	// Use the mux.Handle() function to register the file server as the handler for
+	// all URL paths that start with "/static/". For matching paths, we strip the
+	// "/static" prefix before the request reaches the file server.
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	log.Println("Starting server on :4000")
 	log.Fatal(http.ListenAndServe(":4000", mux))
 
