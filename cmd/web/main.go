6480b24e284369266f2a4a009142cd3d8613cc53
Xao <divxyng.edu@gmail.com>
2021-10-23 12:58:30 -0500

    Refactored routes into routes.go file

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -40,41 +40,12 @@ func main() {
 		and expose them to our users.
 	*/
 
-	// For the sake of this exercise, let's keep using mux.
-	mux := http.NewServeMux()
-
-	// subtree path, patterns are matched
-	mux.HandleFunc("/", app.home)
-
-	// fixed paths
-	mux.HandleFunc("/snippet", app.showSnippet)
-	mux.HandleFunc("/snippets", app.showSnippets)
-	mux.HandleFunc("/snippet/create", app.createSnippet)
-
-	// Create a file server which serves files out of the "./ui/static" directory.
-	// Note that the path given to the http.Dir function is relative to the project
-	// directory root.
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-
-	/*
-		Go's file server is pretty cool. It has a few really nice features:
-		* sanitizes all request paths
-		* range requests are fully supported. You can specify the range of bytes you want.
-		* Last-Modified and If-Modified-Since headers are transparently supported.
-		* Content-Type is automatically set from the file extension
-	*/
-
-	// Use the mux.Handle() function to register the file server as the handler for
-	// all URL paths that start with "/static/". For matching paths, we strip the
-	// "/static" prefix before the request reaches the file server.
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-
 	infoLog.Printf("Starting server on %s", *addr)
 
 	srv := &http.Server{
 		Addr:     *addr,
 		ErrorLog: errorLog,
-		Handler:  mux,
+		Handler:  app.routes(),
 	}
 
 	err := srv.ListenAndServe()
