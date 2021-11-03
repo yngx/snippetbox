2187478f0630da8cfce69b3dc8f90850369f579d
Xao <divxyng.edu@gmail.com>
2021-11-02 22:26:33 -0500

    Added template for models and mysql model

---
+++ pkg/models/mysql/snippets.go
@@ -0,0 +1,31 @@
+package mysql
+
+import (
+	"database/sql"
+
+	// Import the models package that we just created. You need to prefix this with
+	// whatever module path you set up back in chapter 02.02 (Project Setup and Enabling
+	// Modules) so that the import statement looks like this:
+	// "{your-module-path}/pkg/models".
+	"github.com/yngx/snippetbox/pkg/models"
+)
+
+// Define a SnippetModel type which wraps a sql.DB connection pool.
+type SnippetModel struct {
+	DB *sql.DB
+}
+
+// This will insert a new snippet into the database.
+func (m *SnippetModel) Insert(title, content, expires string) (int, error) {
+	return 0, nil
+}
+
+// This will return a specific snippet based on its id.
+func (m *SnippetModel) Get(id int) (*models.Snippet, error) {
+	return nil, nil
+}
+
+// This will return the 10 most recently created snippets.
+func (m *SnippetModel) Latest() ([]*models.Snippet, error) {
+	return nil, nil
+}
