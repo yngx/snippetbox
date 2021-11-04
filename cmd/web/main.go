7f6832ee09ad3400a1bd2c66bbebc0c44efb77b2
Xao <divxyng.edu@gmail.com>
2021-11-03 23:14:30 -0500

    Pass model to application

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -8,11 +8,13 @@ import (
 	"os"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/yngx/snippetbox/pkg/models/mysql"
 )
 
 type application struct {
 	errorLog *log.Logger
 	infoLog  *log.Logger
+	snippets *mysql.SnippetModel
 }
 
 func main() {
@@ -44,6 +46,7 @@ func main() {
 	app := &application{
 		errorLog: errorLog,
 		infoLog:  infoLog,
+		snippets: &mysql.SnippetModel{DB: db},
 	}
 
 	/*
