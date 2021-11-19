01678b6075f1cb4a86750a52874e06b0d56faad4
Xao <divxyng.edu@gmail.com>
2021-11-18 23:41:26 -0600

    added dynamic date to footer

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -5,7 +5,19 @@ import (
 	"fmt"
 	"net/http"
 	"runtime/debug"
+	"time"
 )
+
+// Create an addDefaultData helper. This takes a pointer to a templateData
+// struct, adds the current year to the CurrentYear field, and then returns
+// the pointer.
+func (app *application) addDefaultData(td *templateData, r *http.Request) *templateData {
+	if td == nil {
+		td = &templateData{}
+	}
+	td.CurrentYear = time.Now().Year()
+	return td
+}
 
 func (app *application) render(w http.ResponseWriter, r *http.Request, name string, td *templateData) {
 	// Retrieve the appropriate template set from the cache based on the page name
@@ -22,7 +34,7 @@ func (app *application) render(w http.ResponseWriter, r *http.Request, name stri
 	// Write the template to the buffer, instead of straight to the
 	// http.ResponseWriter. If there's an error, call our serverError helper and then
 	// return.
-	err := ts.Execute(buf, td)
+	err := ts.Execute(buf, app.addDefaultData(td, r))
 	if err != nil {
 		app.serverError(w, err)
 		return
