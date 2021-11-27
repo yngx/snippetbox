ec79e37dd43751fa6d48eeb5657faf682fc66104
Xao <divxyng.edu@gmail.com>
2021-11-27 13:30:00 -0600

    Added middleware to handle panics

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -27,5 +27,5 @@ func (app *application) routes() http.Handler {
 	// "/static" prefix before the request reaches the file server.
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	return app.logRequest(secureHeaders(mux))
+	return app.recoverPanic(app.logRequest(secureHeaders(mux)))
 }
