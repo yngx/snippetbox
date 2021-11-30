505b47e69f9221b44c2753f05cf437423427b23c
Xao <divxyng.edu@gmail.com>
2021-11-29 20:09:54 -0600

    added simple form for uploading input

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -91,7 +91,7 @@ func (app *application) showSnippets(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) createSnippetForm(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Create a new snippet..."))
+	app.render(w, r, "create.page.tmpl", nil)
 }
 
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
